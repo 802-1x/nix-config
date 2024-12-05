351ab95e5c0209390ec36c56803c93908cb90c05
Matthew Burns <96475907+802-1x@users.noreply.github.com>
2024-12-05 14:37:29 +1100

    Create admin.nix

---
+++ admin.nix
@@ -0,0 +1,11 @@
+{
+  users.users.admin = {
+    isNormalUser = true;
+    home = "/home/admin";
+    description = "Administrator";
+    extraGroups = [ "wheel" "networkmanager" ];
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICgxEx1DpRpafCRhEP+8Js68tf6sLF9U3Udge3aXl4uM admin@homelab"
+    ];
+  };
+}
