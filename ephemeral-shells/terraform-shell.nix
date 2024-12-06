9f82f67dd0750595a00276f0a3e0be4c29f3ee58
Matthew Burns <96475907+802-1x@users.noreply.github.com>
2024-12-06 22:15:53 +1100

    Create terraform-shell.nix

---
+++ ephemeral-shells/terraform-shell.nix
@@ -0,0 +1,15 @@
+{ pkgs ? import <nixpkgs> { config.allowUnfree = true; } }:
+
+pkgs.mkShell {
+  buildInputs = [ pkgs.terraform ];
+
+  shellHook = ''
+    echo "This operation's initialisation will take approximately 10 minutes. Do you want to proceed? (y/n)"
+    read -r response
+    if [[ "$response" != "y" ]]; then
+      echo "Exiting shell."
+      exit 1
+    fi
+    alias build='terraform'
+  '';
+}
