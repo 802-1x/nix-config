b6481113a68d03f09ecb0ed4cc4927f10a2d4c9a
Matthew Burns <96475907+802-1x@users.noreply.github.com>
2024-12-08 00:32:14 +1100

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,23 @@
+variable "pm_api_url" {
+  default = "https://[x].[x].[x].[x]:8006/api2/json"
+}
+
+variable "pm_user" {
+  default = "[x]@[y]![z]"
+}
+
+variable "pm_api_token_id" {
+  default = "[x]@[y]![z]"
+}
+
+variable "pm_api_token_secret" {
+  default = "[redacted]"
+}
+
+variable "target_node" {
+  default = "[x].[x].[x].[x]"
+}
+
+variable "template_name_1" {
+  default = "golden-template"
+}
