e20a15a582d2b661ec714f929d80a9d3ceac3b79
Matthew Burns <96475907+802-1x@users.noreply.github.com>
2024-12-08 11:50:29 +1100

    Imported cert and updated tls verification flag

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,6 +1,6 @@
 provider "proxmox" {
   pm_parallel           = 1
-  pm_tls_insecure       = true
+  pm_tls_insecure       = false
   pm_api_url            = var.pm_api_url
   pm_api_token_id       = var.pm_user
   pm_api_token_secret   = var.pm_api_token_secret
