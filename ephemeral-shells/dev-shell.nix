9d4442bc8a8e1d2678bc622031653a89ccf79c66
Matthew Burns <96475907+802-1x@users.noreply.github.com>
2024-12-18 23:44:46 +1100

    feat(dev-shell): expanded to include welcome message and build inputs listing

--- ephemeral-shells/dev-shell.nix
+++ ephemeral-shells/dev-shell.nix
@@ -1,11 +1,22 @@
 { pkgs ? import <nixpkgs> { config.allowUnfree = true; } }:
 
-pkgs.mkShell {
-  buildInputs = [
+let
+  myBuildInputs = [
     pkgs.vscode
     pkgs.smlnj
     pkgs.bandit
     pkgs.python3
     pkgs.python3Packages.nuitka
   ];
+
+  packageNames = builtins.map (pkg: pkg.name) myBuildInputs;
+
+in
+  pkgs.mkShell {
+    buildInputs = myBuildInputs;
+
+  shellHook = ''
+    echo "Welcome to the development shell!"
+    echo "Build inputs: ${builtins.concatStringsSep ", " packageNames}"
+  '';
 }
