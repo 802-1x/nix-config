ba669069fdc39908ec613229678ec551ab80940b
Matthew Burns <96475907+802-1x@users.noreply.github.com>
2024-12-19 20:55:45 +1100

    feat(presentation-shell): added welcome message and build input listing

--- ephemeral-shells/presentation-shell.nix
+++ ephemeral-shells/presentation-shell.nix
@@ -1,12 +1,21 @@
 { pkgs ? import <nixpkgs> {} }:
 
-pkgs.mkShell {
-  buildInputs = [
+let
+  myBuildInputs = [
     pkgs.hugo
   ];
+
+  packageNames = builtins.map (pkg: pkg.name) myBuildInputs;
+
+in
+  pkgs.mkShell {
+    buildInputs = myBuildInputs;
 
   shellHook = ''
     # Append SHELL_TRACKER environment variable
     export SHELL_TRACKER="$SHELL_TRACKER:presentation"
+
+    echo "Welcome to the presentation shell!"
+    echo "Build inputs: ${builtins.concatStringsSep ", " packageNames}"
   '';
 }
