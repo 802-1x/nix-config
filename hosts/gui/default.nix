23018ec66e4b25261b43965e7d8c7bea822e4ca2
Matthew Burns <96475907+802-1x@users.noreply.github.com>
2024-12-23 23:42:38 +1100

    feat(gui): expanded functionality

--- hosts/gui/default.nix
+++ hosts/gui/default.nix
@@ -6,10 +6,23 @@
       ./hardware-configuration.nix
     ];
 
-boot.loader.grub.enable = true;
-boot.loader.grub.device = "/dev/sda";
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/sda";
 
-networking.hostName = "test";
-networking.wireless.enable = true;
+  networking.hostName = "test";
+  networking.wireless.enable = true;
 
-system.stateVersion = "24.11";
+  system.stateVersion = "24.11";
+
+  networking.networkmanager.enable = false;
+
+  services.xserver.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  boot.kernelParams = [ "nouveau.modeset=0" ];
+
+  nixpkgs.config.allowUnfree = true;
+  hardware.nvidia.open = true;
+
+}
