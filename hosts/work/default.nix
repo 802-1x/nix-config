69d3f406414c00251ad38af1361eb3bb6572279b
Matthew Burns <96475907+802-1x@users.noreply.github.com>
2024-12-24 20:17:43 +1100

    chore: cleaned up now modularised code

--- hosts/work/default.nix
+++ hosts/work/default.nix
@@ -6,6 +6,7 @@
       ./hardware-configuration.nix
       ./common/locale.nix
       ./common/current-system-packages.nix
+      ./modules/gnome-hyper-v/default.nix
     ];
 
   # Use the GRUB 2 boot loader
@@ -15,56 +16,6 @@
 
   networking.hostName = "work";
   networking.wireless.enable = true;
-  networking.networkmanager.enable = false;
 
   system.stateVersion = "24.11";
-
-  services.xserver = {
-    enable = true;
-    displayManager.gdm.enable = true;
-    desktopManager.gnome.enable = true;
-    modules = [ pkgs.xorg.xf86videofbdev ];
-  };
-
-  environment.systemPackages = with pkgs; [
-    gnome-remote-desktop
-  ];
-
-  environment.gnome.excludePackages = with pkgs; [
-    cheese
-    epiphany
-    gnome-music
-    gnome-tour
-    gnome-weather
-    yelp
-  ];
-
-  services.gnome.gnome-remote-desktop.enable = true;
-  services.xrdp.enable = true;
-  services.xrdp.defaultWindowManager = "${pkgs.gnome-session}/bin/gnome-session";
-  services.xrdp.openFirewall = true;
-
-  systemd.targets.sleep.enable = false;
-  systemd.targets.suspend.enable = false;
-  systemd.targets.hibernate.enable = false;
-  systemd.targets.hybrid-sleep.enable = false;
-
-  boot.kernelParams = [ "nouveau.modeset=0" ];
-
-  nixpkgs.config.allowUnfree = true;
-  hardware.nvidia.open = true;
-
-  users.groups.gdm = {};
-  users.users.gdm = {
-    isSystemUser = true;
-    group = "gdm";
-    extraGroups = [ "video" ];
-  };
-
-  users.users.admin = {
-    isNormalUser = true;
-    home = "/home/admin";
-    description = "Administrator";
-    extraGroups = [ "wheel" "networkmanager" "tty" "input" "audio" "video" ];
-  };
 }
