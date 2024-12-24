bfe958b946ef04ef2ee08451a580e72f9de0b3da
Matthew Burns <96475907+802-1x@users.noreply.github.com>
2024-12-24 16:46:45 +1100

    chore: removed unnecessary media driver line

--- hosts/work/default.nix
+++ hosts/work/default.nix
@@ -25,7 +25,6 @@
     displayManager.gdm.enable = true;
     desktopManager.gnome.enable = true;
     modules = [ pkgs.xorg.xf86videofbdev ];
-    #videoDrivers = [ "hyperv_fb" "modesetting" "nvida" ];
   };
 
   boot.kernelParams = [ "nouveau.modeset=0" ];
