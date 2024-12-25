2ab85a8cb5ae1979024af3d06c311886448614ea
Matthew Burns <96475907+802-1x@users.noreply.github.com>
2024-12-25 22:28:36 +1100

    feat: removed disablement of sleep settings as no longer required

--- modules/gnome-hyper-v/default.nix
+++ modules/gnome-hyper-v/default.nix
@@ -86,11 +86,6 @@
   services.xrdp.defaultWindowManager = "${pkgs.gnome-session}/bin/gnome-session";
   services.xrdp.openFirewall = true;
 
-  systemd.targets.sleep.enable = false;
-  systemd.targets.suspend.enable = false;
-  systemd.targets.hibernate.enable = false;
-  systemd.targets.hybrid-sleep.enable = false;
-
   boot.kernelParams = [ "nouveau-modeset=0" ];
 
   nixpkgs.config.allowUnfree = true;
