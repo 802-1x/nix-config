28d176a5de09529f1fc91a5e8ce9f6a249cb73e2
Matthew Burns <96475907+802-1x@users.noreply.github.com>
2024-12-26 22:44:14 +1100

    feat(users): add test user account for new system

---
+++ users/work/default.nix
@@ -0,0 +1,16 @@
+{
+  users.users.work = {
+    isNormalUser = true;
+    home = "/home/work";
+    description = "Administrator";
+    extraGroups = [ "wheel" "networkmanager" "tty" "input" "audio" "video" ];
+
+    #openssh.authorizedKeys.keys = [
+    #  ""
+    #];
+
+    packages = with pkgs; [
+      flameshot
+    ];
+  };
+}
