1dbfa3b345490fbdd865bd054b8e72c1f224f75e
Matthew Burns <96475907+802-1x@users.noreply.github.com>
2025-01-03 12:01:33 +1100

    chore: re-organising modules

--- hosts/work/configuration.nix
+++ hosts/work/configuration.nix
@@ -6,9 +6,7 @@
       ./hardware-configuration.nix
       ./common/default.nix
       ./modules/gnome-hyper-v/default.nix
-      ./common/ephemeral-shells/default.nix
-      ./common/hardening/default.nix
-      ./users/work/default.nix
+      ./hardening/baseline.nix
     ];
 
   # Use the GRUB 2 boot loader
@@ -21,13 +19,4 @@
 
   # Never delete or modify this value unless you have thoroughly understood man configuration.nix
   system.stateVersion = "24.11";
-
-  # Environment aliases
-  environment.shellAliases = {
-    sysadmin = "${pkgs.nix}/bin/nix-shell /etc/nixos/common/ephemeral-shells/sysadmin-shell.nix";
-    #dev = "${pkgs.nix}/bin/nix-shell /etc/nixos/ephemeral-shells/dev-shell.nix";
-    #build = "${pkgs.nix}/bin/nix-shell /etc/nixos/ephemeral-shells/build-shell.nix"; 
-    #presentation = "${pkgs.nix}/bin/nix-shell /etc/nixos/ephemeral-shells/presentation-shell.nix";
-    #security = "${pkgs.nix}/bin/nix-shell /etc/nixos/ephemeral-shells/security-shell.nix";
-  };
 }
