ab73d7e3ded2718b79b937c031523d5722e17e75
Matthew Burns <96475907+802-1x@users.noreply.github.com>
2025-05-30 21:03:16 +1000

    feat: refactored to include echo of build inputs

--- common/ephemeral-shells/sysadmin-shell.nix
+++ common/ephemeral-shells/sysadmin-shell.nix
@@ -6,26 +6,34 @@ let
     vim.opt.relativenumber = true
   '';
 
+  myBuildInputs = [
+    pkgs.neovim
+    pkgs.htop
+    pkgs.sysstat
+    pkgs.tmux
+    pkgs.git
+    pkgs.curl
+    pkgs.wget
+    pkgs.man
+    pkgs.jq
+  ];
+
+  packageNames = builtins.map (pkg: pkg.name) myBuildInputs;
+  concatPackageNames = builtins.concatStringsSep ", " packageNames;
+
 in
-  pkgs.mkShell {
-    buildInputs = [
-      pkgs.neovim
-      pkgs.htop
-      pkgs.sysstat
-      pkgs.tmux
-      pkgs.git
-      pkgs.curl
-      pkgs.wget
-      pkgs.man
-      pkgs.jq
-    ];
+pkgs.mkShell {
+  buildInputs = myBuildInputs;
 
   shellHook = ''
     # Append SHELL_TRACKER environment variable
     export SHELL_TRACKER="$SHELL_TRACKER:sysadmin"
 
+    echo -e "\e[36mWelcome to the sysadmin shell!\e[0m"
+    echo -e "\e[32mBuild inputs: ${concatPackageNames}\e[0m"
+
     mkdir -p ~/.config/nvim
     chmod u+w /home/%USER/.config/nvim/ini
     cp ${neovimConfig} ~/.config/nvim/init.lua
-    '';
-  }
+  '';
+}
