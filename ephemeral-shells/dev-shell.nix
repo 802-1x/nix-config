f154651dc3b8f30da93565c06c85e7b1dc458f8a
Matthew Burns <96475907+802-1x@users.noreply.github.com>
2025-06-06 10:54:25 +1000

    feat: added fish terminal

--- ephemeral-shells/dev-shell.nix
+++ ephemeral-shells/dev-shell.nix
@@ -41,6 +41,13 @@ in
     export SHELL_TRACKER="$SHELL_TRACKER:dev"
 
     echo -e "\e[36mWelcome to the development shell!\e[0m"
+    echo -e "\e[36mYou are in a fish-y shell environment.\e[0m"
     echo -e "\e[32mBuild inputs: ${builtins.concatStringsSep ", " packageNames}\e[0m"
+
+    # Must come after above output commands otherwise fish gobbles the current shell process stopping further display output
+    if [ -z "$IN_FISH_DEV" ]; then
+      export IN_FISH_DEV=1
+      exec ${pkgs.fish}/bin/fish
+    fi
   '';
 }
